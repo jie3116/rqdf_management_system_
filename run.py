99a23ed0a30458746c11ca04db775d2d972a1f8e
Aji <aji.smartvision@gmail.com>
2026-01-13 23:51:39 +0700

    Update docker-compose, run.py dan tambah backup config nginx

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+import os
 from app import create_app, db
 from app.models import User, UserRole
-import os
+
 
 app = create_app()
 
@@ -12,3 +13,6 @@ def make_shell_context():
 if __name__ == '__main__':
     debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
     app.run(host='0.0.0.0', port=8000, debug=debug_mode)
+
+
+
