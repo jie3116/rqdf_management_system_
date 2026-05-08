cac5269867250225a56cba0d0c7b6ff7eee92044
Aji <aji.smartvision@gmail.com>
2026-05-08 08:27:34 +0700

    fix(db): merge alembic heads

---
+++ migrations/versions/fa28f14d5f8c_merge_savings_and_userrole_heads.py
@@ -0,0 +1,24 @@
+"""merge savings and userrole heads
+
+Revision ID: fa28f14d5f8c
+Revises: a1b2c3d4e5f6, y5n6o7p8q9r0
+Create Date: 2026-05-08 08:14:20.619882
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fa28f14d5f8c'
+down_revision = ('a1b2c3d4e5f6', 'y5n6o7p8q9r0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
